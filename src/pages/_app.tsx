f25d68429a638753782bd3e2abb5a1d7ab1a2a24
matekaszas13 <matekaszas17@gmail.com>
2024-06-24 17:10:54 +0200

    fix: fix meta tag for initial scale

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="description"
           content="Varga Márton magántanár weboldala. Matematika vizsgafelkészítés és magánoktatás."
