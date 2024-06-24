10ffd2689cc9969e538126ee5a8fb60fec25170b
matekaszas13 <matekaszas17@gmail.com>
2024-06-24 17:06:12 +0200

    feat: add meta tags and icon for tab

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,36 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { appWithTranslation } from "next-i18next";
+import Head from "next/head";
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="Varga Márton magántanár weboldala. Matematika vizsgafelkészítés és magánoktatás."
+        />
+        <meta
+          name="keywords"
+          content="Varga Márton, magántanár, matematika, vizsgafelkészítés, magánoktatás, Budapest, magyar nyelv, Budapest XIII. kerület, Újlipót,
+         legjobb magántanár,oktatás, közgazdász, hallgató, felkészítés, középiskola, egyetem, emelt szintű érettségi, központi felvételi, zárthelyi vizsgák"
+        />
+        <meta name="author" content="Varga Márton" />
+        <link rel="icon" href="/images/logob.png" type="image/x-icon"></link>
+        <link
+          as="font"
+          rel="preload"
+          href="/fonts/Hubot-Sans.woff2"
+          crossOrigin=""
+          type="font/woff2"
+        />
+        <title>Varga Márton Magántanár</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default appWithTranslation(App);
