ca5f589a5bdacd83704724d75e9022acf0290d2e
matekaszas13 <matekaszas17@gmail.com>
2024-09-01 18:19:03 +0200

    chore: add google ads script to head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { appWithTranslation } from "next-i18next";
 import Head from "next/head";
+import Script from "next/script";
 
 function App({ Component, pageProps }: AppProps) {
   return (
@@ -28,6 +29,22 @@ function App({ Component, pageProps }: AppProps) {
         />
         <title>Varga Márton magántanár</title>
       </Head>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=AW-16604278618`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-16604278618');
+            `,
+        }}
+      />
       <Component {...pageProps} />
     </>
   );
